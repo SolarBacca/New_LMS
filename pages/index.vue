6c796a49a78d636b67f5b64136aae444cc46da2e
Ivan Morozov <vanyam.vvo@gmail.com>
2025-12-15 16:27:02 +1000

    project setup done

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1>Добро пожаловать в <b>информационную</b> платформу!</h1>
+</template>
+
+<style>
+b {
+    color: rgb(35, 241, 35)
+}
+</style>
